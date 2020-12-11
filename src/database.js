81835819312d7f71bcac3f41b6b5aef6334fe19f
Felix <felixcano@gmail.com>
2020-12-10 22:01:34 -0600

    version con db remote

--- src/database.js
+++ src/database.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/notas', {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    })
-    .then(db => console.log('Conectado a las base de datos'))
-    .catch(err => console.error(err));
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+if (process.env.NODE_ENV === 'dev') {
+    mongoose.connect('mongodb://localhost/notas', {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        })
+        .then(db => console.log('Conectado a las base de datos'))
+        .catch(err => console.error(err));
+} else {
+    mongoose.connect('mongodb+srv://jorlaf:9Bot2j3XtAazIoUV@cluster0.9axsh.mongodb.net/notas?retryWrites=true&w=majority', {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        })
+        .then(db => console.log('Conectado a las base de datos'))
+        .catch(err => console.error(err));
+}
